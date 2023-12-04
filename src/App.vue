74e62cff8dea8f437d918b59a5d27cfa023a9871
邹小一 <zouxiaoyi@zouxiaoyideMacBook-Pro.local>
2023-12-04 16:30:43 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-red-500">
+    sokoban: {{ map }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useMapStore } from "./store/map";
+
+const { map } = useMapStore()
+</script>
+
+<style scoped>
+</style>
---
+++ src/store/tests/map.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe('map', () => {
+    it('should set map', () => {
+        const map = [1,2,3]
+        expect(map).toEqual([1,2,3])
+    })
+})
